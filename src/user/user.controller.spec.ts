6dfff88c72c2a28ff976dd9a7e98a5e0b16064a2
JosepDev23 <josepdev23@gmail.com>
2023-07-22 01:40:51 +0200

    user tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import User from './user.schema';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [{
+        provide: UserService,
+        useValue: {
+          save: jest.fn()
+        }
+      }]
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService)
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-});
+  })
+
+  describe('postUser', () => {
+    it('should create a new user', async () => {
+      const result: User = {
+        username: 'username',
+        password: 'password',
+        email: 'email',
+        phone: 654654654
+      }
+      jest.spyOn(service, 'save').mockResolvedValue(result)
+
+      expect(await controller.postUser(result)).toBe(result)
+    })
+  })
+})
